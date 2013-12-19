af96d605d9bd3fe962cdd72c3ffdb758956385ce
EricWVGG <eric@whiskyvangoghgo.com>
2013-12-18 19:13:02 -0500

    removed references

--- app/js/app.js
+++ app/js/app.js
@@ -3,8 +3,6 @@
 
 // Declare app level module which depends on filters, and services
 angular.module('life', [
-  'life.filters',
-  'life.services',
   'life.directives',
   'life.controllers'
 ]);
