4928ac2842e91948af17cb4f9c03ad6e8c9972cd
EricWVGG <eric@whiskyvangoghgo.com>
2014-07-12 17:44:26 -0400

    huge optimization

--- js/controllers.js
+++ js/controllers.js
@@ -5,24 +5,36 @@
 angular.module('life.controllers',[]).
 
 
-  controller('GameController',['$scope',function($scope) {
-      $scope.rows = $scope.cols = 20;
-      $scope.grid = [];
-      for( var y = 0; y < $scope.rows; y++ ) {
-          $scope.grid[y] = [];
-          for( var x = 0; x < $scope.rows; x++ ) {
-              $scope.grid[y][x] = 0;
-          }
-      }
-      
-      // replace this with whatever seed you want
-      $scope.grid[3][4] = $scope.grid[3][5] = $scope.grid[3][6] = $scope.grid[2][6] = $scope.grid[1][5] = 1;
-      
-      $scope.tick = function() {
-          $scope.$broadcast('tick');
-      }
-      
-  }])
+    controller('GameController',['$scope', '$timeout', function($scope, $timeout) {
+        $scope.rows = $scope.cols = 60;
+        $scope.grid = [];
+        for( var y = 0; y < $scope.rows; y++ ) {
+            $scope.grid[y] = [];
+            for( var x = 0; x < $scope.rows; x++ ) {
+                $scope.grid[y][x] = 0;
+            }
+        }
+        
+        // replace this with whatever seed you want
+        $scope.grid[3][4] = $scope.grid[3][5] = $scope.grid[3][6] = $scope.grid[2][6] = $scope.grid[1][5] = 1;
+        
+        $scope.speed = 5;
+        var ticktock = true;
+        $scope.tick = function() {
+            (function animloop(){
+                $timeout(function() {
+                    if(ticktock) {
+                        $scope.$broadcast('tick');
+                    } else {
+                        $scope.$broadcast('tock');
+                    }
+                    ticktock = !ticktock;
+                    requestAnimationFrame(animloop);
+                }, $scope.speed);
+            })();
+         }
+        
+    }])
 
 
 
--- js/directives.js
+++ js/directives.js
@@ -6,45 +6,39 @@
 angular.module('life.directives', [])
 
 
-  .directive('dude', ['$timeout', function($timeout) {
-      return {
-          restrict: 'A',
-          scope: {grid: '='},
-          link: function(scope, element, attributes) {
-              var x = scope.$parent.$index;
-              var y = scope.$parent.$parent.$index;
-              var count;
-              scope.state = 0;
-              var tick = function() {
-                  count = 0;
-                  for( var i = (y-1); i < y+2; i++ ) {
-                      if(i < 0 || i >= scope.grid.length) continue; // off the grid
-                      for( var ii = (x-1); ii < x+2; ii++ ) {
-                          if( 
-                            ii < 0 || ii >= scope.grid[i].length // off the grid
-                            || i == y && ii == x // self
-                          ) continue;
-                          if( scope.grid[i][ii] > 0 ) count++;
-                      }
-                  }
-                  $timeout(function() {
-                      if( scope.grid[y][x] > 0 && count < 2 ) scope.grid[y][x] = -1;
-                      else if( scope.grid[y][x] > 0 && ( count == 2 || count == 3 ) ) scope.grid[y][x] = 1;
-                      else if( scope.grid[y][x] > 0 && ( count > 3 ) ) scope.grid[y][x] = -1;
-                      else if( scope.grid[y][x] < 1 && ( count == 3 ) ) scope.grid[y][x] = 1;
-                      else scope.grid[y][x] = 0;
-                      $timeout(function() {
-                        tick();
-                      }, .01);
-                  }, .01);
-              }
-              var running = scope.$on('tick', function() {
-                  tick();
-                  running(); // deregister listener
-              });
-          }
-      }
-  }])
+    .directive('dude', ['$timeout', function($timeout) {
+        return {
+            restrict: 'A',
+            scope: {grid: '='},
+            link: function(scope, element, attributes) {
+                // switch to requestAnimationFrame and watch for broadcasts
+                var x = scope.$parent.$index;
+                var y = scope.$parent.$parent.$index;
+                var count;
+                scope.state = 0;
+                scope.$on('tick', function() {
+                    count = 0;
+                    for( var i = (y-1); i < y+2; i++ ) {
+                        if(i < 0 || i >= scope.grid.length) continue; // off the grid
+                        for( var ii = (x-1); ii < x+2; ii++ ) {
+                            if( 
+                              ii < 0 || ii >= scope.grid[i].length // off the grid
+                              || i == y && ii == x // self
+                            ) continue;
+                            if( scope.grid[i][ii] > 0 ) count++;
+                        }
+                    }
+                });
+                scope.$on('tock', function() {
+                    if( scope.grid[y][x] > 0 && count < 2 ) scope.grid[y][x] = -1;
+                    else if( scope.grid[y][x] > 0 && ( count == 2 || count == 3 ) ) scope.grid[y][x] = 1;
+                    else if( scope.grid[y][x] > 0 && ( count > 3 ) ) scope.grid[y][x] = -1;
+                    else if( scope.grid[y][x] < 1 && ( count == 3 ) ) scope.grid[y][x] = 1;
+                    else scope.grid[y][x] = 0;
+                });
+            }
+        }
+    }])
   
 
 ;
